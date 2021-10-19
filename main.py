df190a03543cd7e7a97ca52b7810dfd0527efcfd
Francisco Pizarro Bianchi <frpizarrob@gmail.com>
2021-10-19 10:17:14 -0300

    a

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
+import subprocess
+from selenium import webdriver
 
-html_text = requests.get('https://www.linio.cl/cm/venta-gamer?skus=ON913EL07QN1ULACL&utm_source=SIS_LI_HoldingBanner-1.1&utm_medium=referral&utm_campaign=Tecnologia-Gamer').text
-soup = BeautifulSoup(html_text,'lxml')
-products = soup.find_all('div', class_='catalogue-product row')
-with open(f'posts/productos.txt','w',encoding='utf-8') as f:
-    for index, product in enumerate(products):
-        tittle = product.find('img')['alt'].replace(' ','')
-        price = product.find('span', class_='price-main-md').text.replace(' ','')
-        f.write(f"Nombre producto: {tittle}")
-        f.write(f"Precio: {price}")
-        print(f'Archivo guardado: {index}')
+#Rotar proxy
+# cmd = 'python proxyrotate.py'
+
+headers = {
+     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+}
+link = 'https://www.jumbo.cl/lacteos-y-bebidas-vegetales'
+
+
+driver = webdriver.Firefox()
+driver.get(link)
+
+soup = BeautifulSoup(driver.page_source,'html.parser')
+
+
+listaProductos = soup.find_all('li', class_='shelf-item')
+print(listaProductos)
---
+++ proxyrotate.py
@@ -0,0 +1,9 @@
+# import requests
+# from bs4 import BeautifulSoup
+
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+# }
+# link = 'https://www.unimarc.cl/lacteos-huevos-y-refrigerados/leches-cremas-y-bebidas-vegetales?page=1'
+
+# r = requests.get(link, headers=headers)
