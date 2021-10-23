0ec4ce76d6eaa8f1a87d68b79dbc75c7673f43cd
FranciscoPizarroBianchi <78566973+FranciscoPizarroBianchi@users.noreply.github.com>
2021-10-23 10:17:17 -0300

    Avance de explicit wait

--- unimarc.py
+++ unimarc.py
@@ -3,33 +3,31 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException,TimeoutException
+
 
 link='https://www.unimarc.cl/desayuno-y-dulces/cereales'    #Enlace a la pagina scrappear
-driver = webdriver.Firefox()
-driver.maximize_window()        #For maximizing window
-driver.implicitly_wait(20)      #Espera como maximo 20 segundos para que cargue la pagina
-driver.get(link)               #
-driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+driver = webdriver.Safari()     #Define el navegador a utilizar
+driver.maximize_window()        #Maximizar ventana
+#driver.implicitly_wait(10)      #Espera como maximo 20 segundos para que cargue la pagina
+driver.get(link)
+driver.execute_script("window.scrollTo(0,document.body.scrollHeight)") #Scrollear toda la pagina, para que termine de cargar
 
 
 #Productos
-#products = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'unimarc-pixels-search-0-x-galleryItem unimarc-pixels-search-0-x-galleryItem--normal pa4')))
-sleep(5)
-wait = WebDriverWait(driver, 100)
+wait = WebDriverWait(driver, 100)   #Espera a que la pagina cargue o se demore 100 segundos
+wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME,'product-item'))) #Espera hasta que hayan cargado todas las "tarjetas" de produtos
 
-wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME,'product-item')))
-
-products = driver.find_elements(By.CLASS_NAME,'product-item')
+products = driver.find_elements(By.CLASS_NAME,'product-item') #Buscar "tarjeta" de cada producto
 for product in products:
-    name = product.find_element(By.CLASS_NAME,'product-title').text
-    brand = product.find_element(By.CLASS_NAME,'brand-name').text
+    name = product.find_element(By.CLASS_NAME,'product-title').text     #Extrae el nombre de cada producto
+    brand = product.find_element(By.CLASS_NAME,'brand-name').text       #Extrae la linea de cada producto
     try:
-        price = product.find_element(By.CLASS_NAME,'product-price').text
-    except NoSuchElementException:
+        price = WebDriverWait(product,2).until(EC.presence_of_element_located((By.CLASS_NAME,'product-price'))).text #Extrae el precio de producto, con la condicion de esperar max 2 segundos - Productos bajados de precio
+    except TimeoutException or NoSuchElementException:
         try:
-            price = product.find_element(By.CLASS_NAME,'product-price-normal').text
-        except:
+            price = WebDriverWait(product,2).until(EC.presence_of_element_located((By.CLASS_NAME,'product-price-normal'))).text #Extrae el precio de producto, con la condicion de esperar max 2 segundos - Productos precio normal
+        except TimeoutException or NoSuchElementException:
             print("Error precio")
-    print(f'Nombre: {name}\nMarca: {brand}\nPrecio: {price}')
-    sleep(1.5)
+    print(f'Nombre: {name}\nMarca: {brand}\nPrecio: {price}')   #Genera print con los datos extaidos
+    sleep(1)
